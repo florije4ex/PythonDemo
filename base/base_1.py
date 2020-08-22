907ebaa3355dcb43100d506a689fcaeeb89f95cf
yangbh <yangbaihua@szy.cn>
2020-08-22 14:38:43 +0800

    添加main方法

--- base/base_1.py
+++ base/base_1.py
@@ -2,20 +2,21 @@
 # @Author: yangbaihua
 # @Date  :  2020/05/21 07:33
 
-#字符串是用''  或  ""括起来的任意文本，
-print("I'm OK")
-#如果字符串内部包含'  " 用\转义
-print("I'm \"aa\" OK!")
-# \n换行   \t 制表符  \\ \
-print("I'm OK! \n And You! \t No Possibal \\")
-#
-print(r"I'm OK! \n And You! \t No Possibal \\")
+if __name__ == '__main__':
+    #字符串是用''  或  ""括起来的任意文本，
+    print("I'm OK")
+    #如果字符串内部包含'  " 用\转义
+    print("I'm \"aa\" OK!")
+    # \n换行   \t 制表符  \\ \
+    print("I'm OK! \n And You! \t No Possibal \\")
+    #
+    print(r"I'm OK! \n And You! \t No Possibal \\")
 
-print("打印\&换行符","\\\n\\")
+    print("打印\&换行符","\\\n\\")
 
-print('''多行显示
-      一行
-两行
-三行''')
+    print('''多行显示
+          一行
+    两行
+    三行''')
 
 

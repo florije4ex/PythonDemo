54a13633d2714b768c557a0526d92c117952de33
yangbh <yangbaihua@szy.cn>
2020-10-19 09:10:54 +0800

    接口描述，补充api路径

--- ztjy-project/redis_operation.py
+++ ztjy-project/redis_operation.py
@@ -26,3 +26,37 @@ class Hms_Redis:
             return redis_keys_delete
         else:
             print("Redis Operation【%s】 Not Supported!!!"%operation)
+
+
+
+@pytest.mark.parametrize("userType", (1, 2, 3))
+    @pytest.mark.parametrize("schoolStatus", (0, 1))
+    @pytest.mark.parametrize("isFlag", (0, 1))
+    def test_update_password_and_clear_cache_check_field(self, fixture_init_data, userType, schoolStatus, isFlag):
+        """
+        :param fixture_init_data:
+        :param userType: 1-'SUPER':超级账号, 2-'MAIN'：主账号, 3-'SUB'：子账号
+        :param schoolStatus: 学校状态，1启用 0禁用
+        :param isFlag: 删除标识1存在0不存在
+        :return:
+        """
+        # 测试数据准备
+        SCHOOL_USER_DB().update_sql(self.userName, is_flag=isFlag, status=schoolStatus, user_type=userType,
+                                    user_pwd=self.md5OldPwd)
+        Hms_Redis().operation_redis(self.redis_key, 'set', value=10)
+
+        dto_params = self.iWSchoolUserFacade.generator_dto_params(userName=self.userName, md5Pwd=self.md5Pwd,
+                                                                  userType=userType)
+        params = self.iWSchoolUserFacade.generator_params(SchoolPasswordDTO=dto_params)
+        result = self.iWSchoolUserFacade.updatePasswordAndClearCache(params=params)
+
+        # 查数据库、缓存数据
+        resultSql = SCHOOL_USER_DB().select_sql(user_name=self.userName)
+        resultRedis = Hms_Redis().operation_redis(self.redis_key, 'exists')
+
+        assert_that(result).is_none()
+        assert_that(resultSql[0]['user_type']).is_equal_to(userType)
+        assert_that(resultSql[0]['status']).is_equal_to(schoolStatus)
+        assert_that(resultSql[0]['is_flag']).is_equal_to(isFlag)
+        assert_that(resultSql[0]['user_pwd']).is_equal_to(self.md5Pwd)
+        assert_that(resultRedis).is_false()
